6574322c54548cebdc0c90cc7dd9757dc2fe4bae
ElVrgs <a.varquispe@gmail.com>
2026-05-18 17:50:11 -0500

    docs: Update convert code of vue to angular

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, XXX');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ThirstySeed-WebApp');
   });
 });
