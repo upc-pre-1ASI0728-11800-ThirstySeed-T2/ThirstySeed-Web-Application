c070683d79034bcbf8a4ce271a17fd9efde051d1
ElVrgs <a.varquispe@gmail.com>
2026-05-18 19:34:37 -0500

    docs: Update only code login, we need screen and logic register and forget-password

--- src/app/iam/login/login.ts
+++ src/app/iam/login/login.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
-export class Login {
+export class LoginComponent {
+  username = '';
+  password = '';
+  rememberMe = false;
+  showPassword = false;
 
+  constructor(private router: Router) {}
+
+  handleSignIn(): void {
+    const user = {
+      username: this.username,
+      password: this.password
+    };
+
+    console.log('Login user:', user);
+
+    localStorage.setItem('authToken', 'demo-token');
+    localStorage.setItem('userId', '1');
+
+    this.router.navigate(['/account']);
+  }
+
+  goToSignUp(): void {
+    this.router.navigate(['/sign-up']);
+  }
+
+  goToForgotPassword(event: Event): void {
+    event.preventDefault();
+    this.router.navigate(['/forgot-password']);
+  }
 }
